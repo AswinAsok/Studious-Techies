859238d95cc0917bde5a4c1c408b75dc3c4958b7
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-13 18:20:16 +0530

    Landing page started

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -5,13 +5,14 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import logo from "../Images/LandingLogo.svg";
 
-import './Landing.css'
+import "./Landing.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "#0FFF95",
-    height: "8.5vh"
+    height: "8.5vh",
   },
 }));
 
@@ -19,6 +20,7 @@ const Landing = () => {
   const classes = useStyles();
   return (
     <div>
+      <div className="nav">
         <AppBar className={classes.root} position="static">
           <Toolbar>
             <Grid
@@ -31,6 +33,24 @@ const Landing = () => {
             </Grid>
           </Toolbar>
         </AppBar>
+      </div>
+      <div className="logo-container">
+        <Grid container direction="column" justify="center" alignItems="center">
+          <img className="logo" src={logo} alt="Image" />
+          <div>
+            <p>
+              Are You Prepared to Explore the world of Tech with other <b>Studious Techies</b>.
+              <br></br>
+              Join us by now lets start <b>Learning</b>
+            </p>
+          </div>
+          <div className="buttons">
+            <button className="logbtns">Sign Up</button>
+            <button className="logbtns"> Login</button>
+          </div>
+          
+        </Grid>
+      </div>
     </div>
   );
 };
