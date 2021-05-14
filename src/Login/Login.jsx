391d936b622876f2946220c05d6d3f23b77331cb
Aswin Asok <aswinasokofficial@gmail.com>
2021-05-14 10:33:01 +0530

    Login Page Done

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
+import Grid from "@material-ui/core/Grid";
 
 const Login = () => {
-    return (
-        <div>
-            Login Page
-        </div>
-    )
-}
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <Grid container direction="column" justify="center" alignItems="center">
+          <div className="login-header">
+            <div className="header">Studious-Techies</div>
+          </div>
 
-export default Login
+          <div className="forms">
+            <div className="username">
+              <div className="label">Username</div>
+              <input value="_aswin_asok_" className="input-field" type="text" />
+            </div>
+
+            <div className="password">
+              <div className="label">Password</div>
+              <input value="***********" className="input-field" type="text" />
+            </div>
+
+            <div className="sbtn">
+              <button className="login-button">Login</button>
+            </div>
+          </div>
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
